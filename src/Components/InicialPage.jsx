21f5e8effd53335ea22fb0f2a939a612f7ecc203
gabrieldezena10 <gabriel.deztor@gmail.com>
2022-03-10 20:56:26 -0300

    Tentativa Requisito 5

--- src/Components/InicialPage.jsx
+++ src/Components/InicialPage.jsx
@@ -1,16 +1,98 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Categories from './Categories';
+import ProductCard from './ProductCard';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class InicialPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productArr: [],
+      seachInput: '',
+      listProductFilter: [],
+      // words: '',
+      // Search: '',
+    };
+  }
+
+  // addText = ({ target }) => {
+  //   console.log(target.value);
+  //   this.setState({
+  //     seachInput: target.value,
+  //   });
+  // }
+
+  // addClickButton = () => {
+  //   this.setState((index) => ({
+  //     Search: index.words,
+  //   }));
+  //   const { words } = this.state;
+  //   getProductsFromCategoryAndQuery(words).then((index) => {
+  //     this.setState({
+  //       words: '',
+  //     });
+  //     this.setState({
+  //       productArr: index,
+  //     });
+  //   });
+  // }
+
+  // handleSearch = ({ target: { value } }) => {
+  //   console.log({ target: { value } });
+  //   const { productArr } = this.state;
+  //   this.setState({ searchInput: value }, () => (
+  //     productArr.filter((searchInput) => {
+  //     })
+  //   ));
+  // }
+
+  // filterProducts = (product) => {
+  //   this.setState({
+  //     productArr: productArr.filter((item) => item !== product),
+  //   });
+  // }
+  componentDidMount() {
+    this.fetchGetProdutcs();
+  }
+
+  handleSearch = () => {
+    const { seachInput, productArr } = this.state;
+    const productFilter = productArr
+      .filter((element) => element.title.includes(seachInput));
+    console.log(productFilter);
+    this.setState({
+      listProductFilter: productFilter,
+    });
+  }
+
+  addText = ({ target }) => {
+    console.log(target.value);
+    this.setState({
+      seachInput: target.value,
+    });
+  }
+
+  fetchGetProdutcs = async () => {
+    const requestProduct = await getProductsFromCategoryAndQuery();
+    console.log(requestProduct.results);
+    this.setState({
+      productArr: requestProduct.results,
+    });
+  }
+
   render() {
+    const { seachInput, listProductFilter } = this.state;
     return (
       <div data-testid="home-initial-message">
         <fieldset>
           <input
             type="text"
             name="name"
+            value={ seachInput }
+            onChange={ this.addText }
           />
+          <button type="button" onClick={ () => this.handleSearch() }>Search</button>
         </fieldset>
         <Link data-testid="shopping-cart-button" to="/shoopingCart">
           <button type="button">
@@ -19,7 +101,9 @@ class InicialPage extends React.Component {
         </Link>
         <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
         <Categories />
-
+        <section>
+          <ProductCard listProductFilter={ listProductFilter } />
+        </section>
       </div>
 
     );
--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
-im
+import PropTypes from 'prop-types';
+// import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class ProductCard extends Component {
+  // constructor() {
+  //   super();
+
+  //   this.state = {
+  //     productArr: [],
+  //   };
+  // }
+
+  // componentDidMount() {
+  //   this.fetchGetProdutcs();
+  // }
+
+  // fetchGetProdutcs = async () => {
+  //   const requestProduct = await getProductsFromCategoryAndQuery();
+  //   this.setState({
+  //     productArr: requestProduct,
+  //   });
+  // }
+
   render() {
-    return(
+    // const { title, thumbnail, price } = this.props;
+    const { listProductFilter } = this.props;
+    return (
+      <div>
+        {(!listProductFilter.length === 0) ? (
+          listProductFilter.map((product) => (
+            <section key={ product.id }>
+              <p>{product.title}</p>
+              <img src={ product.thumbnail } alt={ product.title } />
+              <p>{product.price}</p>
+            </section>
+          ))
+        ) : ('Nenhum produto foi encontrado')}
+      </div>
 
     );
   }
 }
+
+ProductCard.propTypes = {
+  listProductFilter: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default ProductCard;
