5096063de56e2de231c0b3710ccdeffb1353908f
gabrieldezena10 <gabriel.deztor@gmail.com>
2022-03-11 14:47:21 -0300

    Bug Requisito 6

--- src/Components/InicialPage.jsx
+++ src/Components/InicialPage.jsx
@@ -11,16 +11,11 @@ class InicialPage extends React.Component {
       productArr: [],
       seachInput: '',
       categArr: [],
-      // id: '',
-      // listProductFilter: [],
-      // words: '',
-      // Search: '',
     };
   }
 
   componentDidMount() {
     this.fetchCategories();
-    // this.fetchGetProdutcs();
   }
 
   fetchCategories = async () => {
@@ -30,55 +25,6 @@ class InicialPage extends React.Component {
     });
     console.log(productsObj);
   }
-  // addText = ({ target }) => {
-  //   console.log(target.value);
-  //   this.setState({
-  //     seachInput: target.value,
-  //   });
-  // }
-
-  // addClickButton = () => {
-  //   this.setState((index) => ({
-  //     Search: index.words,
-  //   }));
-  //   const { words } = this.state;
-  //   getProductsFromCategoryAndQuery(words).then((index) => {
-  //     this.setState({
-  //       words: '',
-  //     });
-  //     this.setState({
-  //       productArr: index,
-  //     });
-  //   });
-  // }
-
-  // handleSearch = ({ target: { value } }) => {
-  //   console.log({ target: { value } });
-  //   const { productArr } = this.state;
-  //   this.setState({ searchInput: value }, () => (
-  //     productArr.filter((searchInput) => {
-  //     })
-  //   ));
-  // }
-
-  // filterProducts = (product) => {
-  //   this.setState({
-  //     productArr: productArr.filter((item) => item !== product),
-  //   });
-  // }
-  // componentDidMount() {
-  //   this.fetchGetProdutcs();
-  // }
-
-  // handleSearch = () => {
-  //   const { seachInput, productArr } = this.state;
-  //   const productFilter = productArr
-  //     .filter((element) => element.title.includes(seachInput));
-  //   console.log(productFilter);
-  //   this.setState({
-  //     listProductFilter: productFilter,
-  //   });
-  // }
 
   addText = ({ target }) => {
     console.log(target.value);
@@ -90,16 +36,16 @@ class InicialPage extends React.Component {
   fetchGetProdutcs = async () => {
     const {
       seachInput,
-      categArr: category,
+      categArr: productsObj,
     } = this.state;
     if (seachInput.length > 0) {
-      const request = await getProductsFromCategoryAndQuery(category.id, seachInput);
+      const request = await getProductsFromCategoryAndQuery(productsObj.id, seachInput);
       console.log(request);
       this.setState({
         productArr: request.results,
       });
     } else {
-      const request = await getProductsFromCategoryAndQuery(category.id, category.name);
+      const request = await getProductsFromCategoryAndQuery(productsObj.id, productsObj.name);
       console.log(request);
       this.setState({
         productArr: request.results,
@@ -130,7 +76,7 @@ class InicialPage extends React.Component {
         </fieldset>
         <Link data-testid="shopping-cart-button" to="/shoopingCart">
           <button type="button">
-            CA
+            CC
           </button>
         </Link>
         <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
---
+++ src/Components/ProductsDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ProductsDetails extends React.Components {
+  render() {
+    return (
+      <div>
+        <p data-testid="product-detail-name">{nome}</p>
+        <button>CC</button>
+      </div>
+    )
+  }
+}
+
+export default ProductsDetails;
